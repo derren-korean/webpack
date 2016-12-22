a4ba55d07586d6a19268235e00daef12bf3a1679
jongwonschoi <jongwons.choi@gmail.com>
2016-12-23 07:13:58 +0900

    apply home

--- template/src/App.vue
+++ template/src/App.vue
@@ -1,28 +1,120 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+    <menus></menus>
+
+    <main>
+      <div id="body">
+        <router-view></router-view>
+      </div>
+    </main>
+
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+import { mapGetters } from 'vuex'
+import Menus from './menu/Menus'
 
 export default {
-  name: 'app',
+  
+  name: '{{name}}-app',
+  
   components: {
-    Hello{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
-  }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
-}{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+    Menus
+  },
+
+  computed: {
+    ...mapGetters({
+      user:"user"
+    })
+  },
+
+  created() {
+    let self = this;
+    
+    if(!self.user || !self.user.userId){
+      self.$router.push({name:'login'});
+      return;
+    }
+
+    self.$router.push('myhome');
+  },
+
+  mounted() {
+
+  },
+
+  beforeDestroy:function(){
+
+  },
+
+  methods:{
+    
+  },  
+}
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+#app {}
+
+.body {
+  .collection {
+    margin: 0px;
+
+    .collection-item {
+      width:100%;
+      text-align:left;
+      color:black;
+    }
+  }
+
+  .collection-ul {
+      margin: 0px;
+
+      .collection-item-li {
+      width:100%;
+      text-align:left;
+      color:black;
+    }
+  }
+
+}
+
+.screen-center {
+  width:100%;
+}
+
+.always-bottom {}
+
+.userView {}
+
+#loading-bar {
+  display:flex;
+  padding-bottom:20%;
+  justify-content: center;
+  align-items:center;
+  width:100%;
+  height:100%;
+}
+
+
+.loadingbar-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 80vh;
+
+  &.screen-center {
+    position: absolute;
+    width: 240px;
+    height: 200px;
+    top: calc(50% - 100px);
+    left: calc(50% - 120px);
+    background-color: white;
+    border: 2px solid rgb(234, 234, 234);
+    border-radius: 1em;
+  }
+
 }
 </style>
